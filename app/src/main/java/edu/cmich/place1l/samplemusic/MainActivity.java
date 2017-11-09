105def694feef00000d541bb576d8f842f34708d
Grose1bm <benjamin.groseclose@hotmail.com>
2017-11-09 12:18:01 -0500

    Started creating the basic UI
     Added 8 sound buttons
     Added Pad Select button
     Added Save and Load buttons
     Then added a textView containing the App Name.
    
     Design corresbonds to specs created with slight variation. Created a file for horizontal UI.

--- app/src/main/java/edu/cmich/place1l/samplemusic/MainActivity.java
+++ app/src/main/java/edu/cmich/place1l/samplemusic/MainActivity.java
@@ -3,6 +3,9 @@ package edu.cmich.place1l.samplemusic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+// Ben testing commit through Android Studio.
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
