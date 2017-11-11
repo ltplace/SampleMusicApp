e40e03aee826285533e26ce33e2322ce129d0591
Grose1bm <benjamin.groseclose@hotmail.com>
2017-11-11 14:17:35 -0500

    Start to set up Firebase.

--- app/src/main/java/edu/cmich/place1l/samplemusic/MainActivity.java
+++ app/src/main/java/edu/cmich/place1l/samplemusic/MainActivity.java
@@ -3,6 +3,8 @@ package edu.cmich.place1l.samplemusic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.storage.FirebaseStorage;
+
 
 // Ben testing commit through Android Studio.
 
@@ -12,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
     }
 }
