3276c740ce2e60998df3b6a5e36a8d2f819a4d26
Grose1bm <benjamin.groseclose@hotmail.com>
2017-11-20 11:55:30 -0500

    continued to create and format the second page. Added code that allows you to go the page.

--- app/src/main/java/edu/cmich/place1l/samplemusic/SampleLoadingPage.java
+++ app/src/main/java/edu/cmich/place1l/samplemusic/SampleLoadingPage.java
@@ -1,5 +1,6 @@
 package edu.cmich.place1l.samplemusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class SampleLoadingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_loading_page);
+
+        Intent intent = getIntent();
     }
 }
